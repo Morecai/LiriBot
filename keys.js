e038e5d81ff87635672171d88a52a8d7eab1a1c4
Frank Mcdonald <frankmcd@cox.net>
2017-06-24 05:31:48 -0700

    tweet and movie info display

--- keys.js
+++ keys.js
@@ -1,14 +1,15 @@
 console.log('this is loaded');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+module.exports = new Twitter({
 
-exports.twitterKeys = {
     consumer_key: '8WH3YJnxeGLtArkP5EIq7OZS8',
     consumer_secret: 'Yv1yEF9oXQj52AjD33WgeMbZ7Opz5kTuO63Hz1ArpMshepLkJb',
     access_token_key: '876968373145542656-NcU968Qmxc4SFPszmk0qWnA5BfLDMW6',
     access_token_secret: 'xrSN0I7anNDQAA9mbUQl173i3rGDxrj4pUw1PLPkDfCkP',
-}
+})
 
-// Spotify
-// Client ID
-// 2495dde7318149058e2fb160f4460a6f
-// Client Secret
-// 8ab3116598b74e1690256482f9ee5123
+// var spotify = new Spotify({
+//     id: '2495dde7318149058e2fb160f4460a6f',
+//     secret: '8ab3116598b74e1690256482f9ee5123'
+// });
--- liri.js
+++ liri.js
@@ -1,33 +1,36 @@
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
 var request = require("request");
+var fs = require('fs');
+var client = require('./keys.js');
+var spotify = require('./keys.js');
+require('dotenv').config();
 var movieName = process.argv[2];
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-console.log(queryUrl);
-request(queryUrl, function(error, response, body) {
+var query = "http://www.omdbapi.com/?t=" + (movieName || 'Mr. Nobody') + "&y=&plot=short&apikey=40e9cece";
+
+request(query, function(error, response, body) {
     if (!error && response.statusCode === 200) {
         console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Rating: " + JSON.parse(body).Rated);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Country produced: " + JSON.parse(body).Production);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[2]);
+
     }
 });
+
 var params = {
-    screen_name: 'nodejs'
+    screen_name: 'banecart'
 };
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
+client.get('statuses/user_timeline', params, function(error, tweet) {
     if (!error) {
-        console.log(tweets);
+        tweet.forEach(function(obj) {
+            console.log('Tweet: ' + obj.text + '\n' + obj.created_at);
+
+        });
+
+    } else {
+        console.log("error");
     }
 });
-
-// var spotify = new Spotify({
-//     id: < your spotify client id >,
-//     secret: < your spotify client secret >
-// });
-
-// spotify
-//     .search({type: 'track', query: 'All the Small Things'})
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (err) {
-//         console.log(error);
-//     });
